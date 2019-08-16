4f14e9b6afae0d52db0b1649d84b680fb5818c6b
xiaoxx <me@xiaoxx.cc>
2019-08-16 17:34:48 +0800

    make TRAVIS_TAG define outside the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-pip install AdvancedHTMLParser --user
-export TRAVIS_TAG=`python getlink.py ver`
-
 # http://www.miui.com/download-330.html
 # Rom URLs
 declare -a urls=(`python getlink.py cn`)
